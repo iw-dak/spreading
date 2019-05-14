bbe5cfd3fc16e379beefb8de969e65c8a99ede63
Adam Malick sow <dachic@Adams-MacBook-Pro.local>
2019-05-14 16:03:23 +0200

    tests on database

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates_presence_of :firstname, :lastname, :birthdate, :address, :email, :phone, :password, :roles ,:posts
+    validates_presence_of :firstname, :lastname, :birthdate, :address, :email, :phone, :password, :roles
 
     has_many :posts
 end
