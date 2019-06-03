57ad88225efdac9d325d63e856ae8eab428ed055
Kaba CONDE <kabaconde15@gmail.com>
2019-06-03 02:00:34 +0200

    Add boostrap + jquery + popper.js

---
+++ front/src/_App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+        Point d'entrée de l'application
+    </div>
+  );
+}
+
+export default App;
